93920fb15bb22699c05561d0d0e63a4de83534fa
Joe Anderson <joe@osbert.me>
2021-03-15 18:35:36 +0000

    Add Join game button in breadcrumbs

--- app/javascript/Application.jsx
+++ app/javascript/Application.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Rails from '@rails/ujs'
+import { Popover } from 'bootstrap'
 import GameChannel from './channels/game_channel'
 import Game from './Game'
 import CopyButton from './CopyButton'
@@ -8,6 +10,7 @@ class Application extends React.Component {
   constructor(props) {
     super(props)
 
+    this.joinGameLinkRef = React.createRef()
     this.gameRef = React.createRef()
     this.jitsiMeetContainerRef = React.createRef()
 
@@ -25,20 +28,44 @@ class Application extends React.Component {
   componentDidMount() {
     if (this.state.onlineGame) {
       this.subscribeToGame()
+    } else {
+      const popoverTarget = this.joinGameLinkRef.current
+      const popoverContent = document.createElement('div')
+      const inputRef = React.createRef()
+
+      const popover = new Popover(popoverTarget, {
+        container: 'body',
+        html: true,
+        sanitize: false,
+        content: popoverContent,
+      })
+
+      ReactDOM.render(
+        <input
+          ref={inputRef}
+          type="text"
+          className="form-control"
+          placeholder="Game link"
+          onChange={
+            event => {
+              const { value } = event.target
+              const matches = value.match(/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/)
+
+              if (matches !== null) {
+                popover.hide()
+                this.setGameId(matches[0])
+              }
+            }
+          } />,
+        popoverContent
+      )
+
+      popoverTarget.addEventListener('shown.bs.popover', () => {
+        inputRef.current.focus()
+      })
     }
 
     window.addEventListener('beforeunload', () => this.setState({ windowUnloading: true }))
-  }
-
-  shareGame() {
-    this.createGame()
-      .then(gameId => {
-        this.setState({
-          onlineGame: true,
-          gameId,
-        }, this.subscribeToGame.bind(this))
-      })
-      .catch(console.error)
   }
 
   createGame() {
@@ -56,10 +83,17 @@ class Application extends React.Component {
       }),
     })
       .then(response => response.json())
-      .then(game => {
-        window.history.replaceState({}, '', `/${game.id}`)
-        return game.id
-      })
+      .then(game => this.setGameId(game.id))
+      .catch(console.error)
+  }
+
+  setGameId(gameId) {
+    window.history.replaceState({}, '', `/${gameId}`)
+
+    this.setState({
+      onlineGame: true,
+      gameId: gameId,
+    }, this.subscribeToGame.bind(this))
   }
 
   subscribeToGame() {
@@ -132,6 +166,36 @@ class Application extends React.Component {
     return (
       <>
         <div className="container-fluid" style={{ maxWidth: '576px' }}>
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb flex-nowrap mb-2">
+              {
+                this.state.onlineGame
+                  ? (
+                    <>
+                      <li key="game" className="breadcrumb-item">
+                        <a href="/" className="text-muted">Game</a>
+                      </li>
+
+                      <li key="game-id" className="breadcrumb-item active text-muted text-truncate">
+                        {this.state.gameId}
+                      </li>
+                    </>
+                  )
+                  : (
+                    <li key="join-game" className="breadcrumb-item active">
+                      <a
+                        ref={this.joinGameLinkRef}
+                        tabIndex="0"
+                        role="button"
+                        className="text-muted">
+                        Join game
+                      </a>
+                    </li>
+                  )
+              }
+            </ol>
+          </nav>
+
           {
             (this.state.shouldBeConnected && !this.state.connected && !this.state.windowUnloading) && (
               <div className="alert alert-danger" role="alert">
@@ -182,7 +246,7 @@ class Application extends React.Component {
                   <p className="lead">You are playing locally</p>
 
                   <div className="d-grid d-md-block gap-2">
-                    <button className="btn btn-dark" onClick={this.shareGame.bind(this)}>
+                    <button className="btn btn-dark" onClick={this.createGame.bind(this)}>
                       Play online
                     </button>
                   </div>
