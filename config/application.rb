74726a9e65bf80cccf3fa448a66357d658195cfb
Joe Anderson <joe@osbert.me>
2022-01-25 18:27:20 +0000

    Add rack_authorised_proxy middleware

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Triangles
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.insert_before 0, Rack::AuthorisedProxy
   end
 end
