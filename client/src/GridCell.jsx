693de65e2c64bc1146f0b4fb6b79de29c162c2ae
Joe Anderson <joe@mousetrapped.co.uk>
2022-07-17 15:12:11 +0100

    Adjust grid colours

--- client/src/GridCell.jsx
+++ client/src/GridCell.jsx
@@ -1,16 +1,15 @@
 import { h } from 'preact'
 
 const GridCell = ({ shape = null, nextShape, ...otherProps }) => {
-  const enabledClass = `bg-slate-100 dark:bg-slate-600 hocus:bg-slate-200 hocus:after:bg-${nextShape} hocus:after:opacity-50 dark:hocus:bg-slate-700`
-  const disabledClass = 'bg-slate-300 dark:bg-slate-800'
+  const emptyClass = `bg-slate-200 dark:bg-slate-700 hocus:bg-slate-100 hocus:after:bg-${nextShape} hocus:after:opacity-40 dark:hocus:bg-slate-800`
 
   const shapeClass =
-    shape === null ? enabledClass : `bg-${shape} ${disabledClass}`
+    shape === null ? emptyClass : `bg-${shape} bg-slate-100 dark:bg-slate-800`
 
   return (
     <button
       type="button"
-      class={`grid-cell relative aspect-square rounded bg-contain bg-no-repeat ring-offset-slate-300 after:absolute after:inset-0 after:bg-contain after:bg-no-repeat dark:ring-offset-slate-800 ${shapeClass}`}
+      class={`grid-cell relative aspect-square rounded bg-contain bg-no-repeat ring-offset-slate-100 after:absolute after:inset-0 after:bg-contain after:bg-no-repeat dark:ring-offset-slate-800 ${shapeClass}`}
       disabled={shape !== null}
       aria-live="polite"
       aria-label={shape === null ? `Play ${nextShape}` : `Square with ${shape}`}
