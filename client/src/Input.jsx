0713e930ae1bf602eefbe9e9eebedad92acef666
Joe Anderson <joe@mousetrapped.co.uk>
2022-07-23 09:44:30 +0100

    Make text inputs appearance: none

--- client/src/Input.jsx
+++ client/src/Input.jsx
@@ -4,7 +4,7 @@ const Input = elem({
   tag: 'input',
   defaultProps: { type: 'text' },
   defaultClass:
-    'p-2 border dark:border-slate-700 rounded-lg bg-white dark:bg-slate-900',
+    'appearance-none p-2 border dark:border-slate-700 rounded-lg bg-white dark:bg-slate-900',
 })
 
 export { Input }
